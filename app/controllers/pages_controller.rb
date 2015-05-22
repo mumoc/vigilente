0a96b479e2072899d9445a6792ebc861942b5928
Jorge Pardiñas <jparbros@gmail.com>
2015-05-22 16:25:14 -0500

    Pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+  def privacy
+
+  end
+
+  def terms
+
+  end
+
+  def support
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   apipie
+
   namespace :api do
     namespace :v1 do
       resources :reports, defaults: { format: 'json' }
     end
   end
+
+  get '/privacy', to: 'pages#privacy'
+  get '/terms', to: 'pages#terms'
+  get '/support', to: 'pages#support'
 end
