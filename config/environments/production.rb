9ecca7de1c55e6aac7f3adc70389aa4ea2777df4
Jorge Pardiñas <jparbros@gmail.com>
2015-06-02 17:00:15 -0500

    Adding CDN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = "assets%d.vigilente.mx.global.prod.fastly.net/"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -86,3 +86,5 @@ Rails.application.configure do
     :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
 end
+
+
